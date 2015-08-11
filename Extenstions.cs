32bcf81245c30a0d0c3455a7e59781f3fba25c09
Djenad Razic <djenad.razic@upandrunningsoftware.com>
2015-08-11 14:59:39 +0200

    - Added extension to clone list  - Added FOMAP Save

---
+++ Extenstions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FOCommon
+{
+    internal static class Extenstions
+    {
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+    }
+}
